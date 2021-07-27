9a1df8c0fb09887b159874bd23ca5f26eb51b5d5
Karapetyan Ashot <ashot.karapetyan.y@gmail.com>
2021-07-27 19:40:40 +0400

    homework done

---
+++ index.js
@@ -0,0 +1,29 @@
+const process = require('process');
+const fs = require('fs')
+const zlib = require("zlib")
+const path = require("path")
+const argv = process.argv.slice(2)
+const zip = zlib.createGzip()
+
+fs.mkdir(path.join(__dirname, 'newFolder'), (err) => {
+    if (err) {
+        return console.error(err);
+    }
+
+    fs.readdir(argv[0], function (err, files) {
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        files.forEach(function (file) {
+            const input = fs.createReadStream(file);
+            const output = fs.createWriteStream("./newFolder/files.gzip");
+
+            input.pipe(zip).pipe(output)
+        });
+    });
+
+    console.log('Directory created successfully!');
+});
+
+
+
